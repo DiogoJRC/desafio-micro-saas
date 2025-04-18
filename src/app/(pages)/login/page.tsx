a788266c885d4232c9f833d947606097e9470ca1
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-04-18 12:40:43 -0300

    feat: add sign with Google using `Auth.js`

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,15 +1,27 @@
+import { handleAuth } from "@/app/actions/handle-auth";
+import Image from "next/image";
+
 export default function Login() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="text-3xl font-bold">Login</h1>
 
-        <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-          href="/login"
-        >
-          LOGIN
-        </a>
+        <form action={handleAuth}>
+          <button
+            type="submit"
+            className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+          >
+            Signin with Google
+            <Image
+              loading="lazy"
+              width={24}
+              height={24}
+              src="google.svg"
+              alt="Google"
+            />
+          </button>
+        </form>
       </main>
     </div>
   );
